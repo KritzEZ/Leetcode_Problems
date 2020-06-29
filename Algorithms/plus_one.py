546343fd5c96378346c5dc141bca9eaa1f0ad64a
KritzEZ <kritarthshah1@gmail.com>
2020-06-28 22:59:51 -0400

    Updated PlusOne

--- Algorithms/plus_one.py
+++ Algorithms/plus_one.py
@@ -1,15 +1,20 @@
 class Solution(object):
     def plusOne(self, digits):
-        for i in range(len(digits)):
-            if digits[len(digits)-1-i] < 9:
-                digits[len(digits)-1-i] += 1
-                return digits
-            else:
-                digits[len(digits)-1-i] = 0
-
-            digits.insert(0, 1)
-            return digits
-            
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """        
         
-
-            
+        if digits[len(digits)-1] != 9:
+            digits[len(digits)-1] += 1
+            return digits
+        
+        else:
+            carry = True
+            for i in range(len(digits)-1, -1, -1):
+                if digits[i] != 9:
+                    digits[i] += 1
+                    return digits   
+                digits[i] = 0
+                if i == 0: digits.insert(0, 1)
+        return digits
