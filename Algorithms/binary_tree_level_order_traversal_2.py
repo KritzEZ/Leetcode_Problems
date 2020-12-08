1fd1fc8a63e5cb01695b16f0349234ea12fb90f7
KritzEZ <kritarthshah1@gmail.com>
2020-12-07 23:05:13 -0500

    Added Binary Tree Level Order Traversal 2

---
+++ Algorithms/binary_tree_level_order_traversal_2.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root:
+            return self.helper([root], [])
+        else: 
+            return []
+        
+        
+    def helper(self, queue, result):
+        next_level = []
+        temp = []
+        for i in queue:
+            temp.append(i.val)
+            if i.left:
+                next_level.append(i.left)
+            if i.right:
+                next_level.append(i.right)
+        
+        result = [temp] + result
+        if not next_level:
+            return result
+    
+        return self.helper(next_level, result)
