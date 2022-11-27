0b4d88f60335de9c4b9ef2f040aa22c83241884d
Kritarth Shah <k38shah@scs.ryerson.ca>
2022-11-27 00:47:23 -0500

    Added Ugly Numbers

---
+++ Algorithms/ugly_number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        ugly = [2,3,5]
+        count = 0
+
+        if n<=0:
+            return False
+
+        for uglyfactor in ugly:
+            while n%uglyfactor==0:
+                n=n//uglyfactor
+
+        return (n==1)
